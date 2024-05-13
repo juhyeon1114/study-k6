576944e17cc53c8f5034d7661ba46dd9938c8e86
juhyeon <juhyeon1114@gmail.com>
2024-05-13 20:34:15 +0900

    some tests

---
+++ common.js
@@ -0,0 +1,4 @@
+export const OPTIONS = {
+    vus: 200,
+    duration: '5s',
+};
---
+++ sleep.js
@@ -0,0 +1,8 @@
+import http from 'k6/http';
+import {OPTIONS} from "./common.js";
+
+export const options = OPTIONS;
+
+export default function () {
+  http.get('http://localhost:8080/hello/sleep');
+}
